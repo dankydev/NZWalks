67acd622f7f6d5b3b03c4b17ca990d983f83eb9e
Daniele Manicardi <manicardi215@gmail.com>
2022-04-14 16:15:36 +0200

    End of Section 2

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Modules.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        {
+
+        }
+
+        // Important: with this DbSet, you're telling to EF to create that table if it doesn't exist and to "talk" with those persistent data
+        public DbSet<Region> Region { get; set; }
+        public DbSet<Walk> Walk { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
